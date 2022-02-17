d85beb8ba4d7e2cf1ba80c054e278d2ee63f944e
Sumna Islam <sumanislam1416@gmail.com>
2022-02-17 15:14:32 +0600

    create userModel, and add functionality to push user to db without hashing the password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,23 @@
+const userModel = require('../models/User')
+
 const signupGET = (req, res) => {
   res.render('signup')
 }
 const loginGET = (req, res) => {
   res.render('login')
 }
-const signupPOST = (req, res) => {
+const signupPOST = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send(req.body)
+  
+  try {
+    const user = await userModel.create({ email, password})
+    res.status(201).json(user);
+  } catch(err) {
+    console.log(err);
+    res.status(400).json('error, user is not created');
+  }
 }
-const loginPOST = (req, res) => {
+const loginPOST = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   res.send(req.body)
